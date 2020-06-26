08bdc30865cdc388149ad25ced766631c9391544
Nikita <komkovnk7@gmail.com>
2020-06-26 14:25:42 +0300

    first-commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*const obj = {
+    name: 'John',
+    age: 18,
+    isMarried:false
+};
+
+console.log(obj.name);
+*/
+
+// const answers = [];
+
+// answers[0] = prompt("Как вас зовут?", "");
+// answers[1] = prompt("Ваша фамилия:", "");
+// answers[2] = +prompt("Сколько вам лет?", "");
+
+// console.log(answers);
+
+const category = 'cats';
+console.log(`https://someurl.com/${category}`);
+
+const name = "Nikita";
+alert(`Привет, ${name}`);
+
